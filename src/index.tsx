1ace4eb47f5a10d65077069caeda62522d0b7b0e
yambal <jy@yambal.net>
2023-08-29 17:03:20 +0900

    Global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './style/theme';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import {normalize} from 'polished';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles
+      styles={normalize()}
+    />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
