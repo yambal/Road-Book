541ba802f85aa85d4307ff32f2b09d35109067a1
yambal <jy@yambal.net>
2023-09-06 23:53:48 +0900

    event handler

--- src/features/mapView/gpsLogView/parts/PointMaker.tsx
+++ src/features/mapView/gpsLogView/parts/PointMaker.tsx
@@ -1,15 +1,21 @@
 import { LatLngExpression, Icon } from "leaflet"
+import React from "react"
 import { Marker } from "react-leaflet"
+import { useAppDispatch } from "../../../../app/hooks"
+import { set as setCurrentFeatureId } from "../../gpsLogViewSlice"
 
 export type PointMakerProps = {
+  id: string
   latLang: LatLngExpression
   opacity?: number
 }
 
 export const PointMaker = ({
+  id,
   latLang,
   opacity = 1
 }: PointMakerProps) => {
+  const dispatch = useAppDispatch()
 
   Icon.Default.mergeOptions({
     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
@@ -17,7 +23,13 @@ export const PointMaker = ({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
   });
 
+  const clickHandler = React.useCallback(() => {
+    dispatch(setCurrentFeatureId(id))
+  }, [id,dispatch])
+
   return (
-    <Marker position={latLang} opacity={opacity}/>
+    <Marker position={latLang} opacity={opacity} eventHandlers={{
+      click: clickHandler
+    }}/>
   )
 }
