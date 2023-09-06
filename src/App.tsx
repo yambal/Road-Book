0e70db4b48966ed9d23155b460d7fa0a3344bf28
yambal <jy@yambal.net>
2023-09-06 23:33:22 +0900

    hide debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { FileLoader } from './features/fileLoader/FileLoader';
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import { Layout } from './components/Layout';
 import { DrawerProvider } from './providers/DrawerProvider';
-import { GlobalStyles, ThemeProvider } from '@mui/material';
+import { GlobalStyles, ThemeProvider, css } from '@mui/material';
 import { theme } from './style/theme';
 import { normalize } from 'polished';
 import { Router } from './routers/Router';
 import { DrawerRouter } from './routers/DrawerRouider';
 
-
+const globalCss = css(normalize(), {
+  iframe: {
+    display: 'none'
+  }
+})
 
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyles
-        styles={normalize()}
+        styles={globalCss}
       />
       <ThemeProvider theme={theme}>
         <DrawerProvider>
